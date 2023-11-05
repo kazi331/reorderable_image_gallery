e9d24660690f15ba1c18df0111a1229d04a57b55
kazi331 <kazisharifulislam52.com>
2023-11-05 17:04:14 +0600

    selected item checkbox and scale modified

--- src/components/GridItem.tsx
+++ src/components/GridItem.tsx
@@ -15,12 +15,12 @@ const GridItem = ({ item, handleSelection, selected }: propsType) => {
     const style = {
         transform: CSS.Transform.toString(transform),
         transition,
-        zIndex: active?.id === item.id ? 999 : undefined, // higher zIndex for the active item to make sure it is always on top while dragging
-        boxShadow: active?.id === item.id ? '0 0 10px 3px rgba(0,0,0,0.2)' : undefined, // add a shadow to the active item
+        zIndex: active?.id === item.id ? 999 : undefined,
+        boxShadow: active?.id === item.id ? '0 0 10px 3px rgba(0,0,0,0.2)' : undefined,
     }
     const labelStyle = active?.id === item.id ? {
         background: 'transparent',
-
+        cursor: 'grabbing'
     } : undefined;
     return (
         <div
@@ -33,8 +33,18 @@ const GridItem = ({ item, handleSelection, selected }: propsType) => {
             <div className={styles.item} >
                 <img src={item.image} alt="Image title goes here..." />
             </div>
-            <input type="checkbox" checked={selected.includes(item.id)} id={item.id.toString()} onChange={() => handleSelection(item.id)} />
-            <label style={labelStyle} htmlFor={item.id.toString()} />
+            <input type="checkbox"
+                checked={selected.includes(item.id)}
+                id={item.id.toString()}
+                onChange={() => handleSelection(item.id)}
+            />
+            <label style={labelStyle} htmlFor={item.id.toString()}>
+                <span className={styles.check}>
+                    <span className={styles.checkbg}>
+                        <svg xmlns="http://www.w3.org/2000/svg" className={styles.checkmark} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" ><path d="M22 11.08V12a10 10 0 1 1-5.93-9.14" /><path d="m9 11 3 3L22 4" /></svg>
+                    </span>
+                </span>
+            </label>
         </div>
     )
 }
