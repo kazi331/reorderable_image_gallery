dc1a43cd43aa870c2f35561f964505ede842c403
kazi331 <kazisharifulislam52.com>
2023-11-06 00:39:21 +0600

    improved responsive

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,16 +9,18 @@ const Header = ({ selected, handleDelete, selectAll, checked }: propsType) => {
 
     return (
         <div className={styles.bar}>
-            <div className={styles.flex} style={{ transform: `${selected.length > 0 ? 'translateY(0.5rem)' : 'translateY(-2.5rem)'}` }}>
+            <div className={styles.flex} style={{ transform: `${selected.length > 0 ? 'translateY(0.5rem)' : 'translateY(-2.9rem)'}` }}>
                 <div className={styles.bar_container}>
                     <div className={styles.bar_selected}>
                         <input id="check" type="checkbox" onChange={selectAll} checked={checked} />
-
                         <p>{selected.length} {selected.length > 1 ? "Files" : "File"} Selected</p>
                     </div>
                     <button disabled={selected.length < 1} onClick={handleDelete}  >Delete Selected</button>
                 </div>
-                <h3 className={styles.title}>Gallery</h3>
+                <h4 className={styles.title}>
+                    <p>Gallery</p>
+                    <button onClick={selectAll} >Select All</button>
+                </h4>
             </div>
 
         </div>
