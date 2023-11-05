b3b16e558132059c74b49e6d3387839c706874d4
kazi331 <kazisharifulislam52.com>
2023-11-05 17:17:53 +0600

    removed unnecessary z-index and changed check icon

--- src/components/GridItem.tsx
+++ src/components/GridItem.tsx
@@ -41,7 +41,8 @@ const GridItem = ({ item, handleSelection, selected }: propsType) => {
             <label style={labelStyle} htmlFor={item.id.toString()}>
                 <span className={styles.check}>
                     <span className={styles.checkbg}>
-                        <svg xmlns="http://www.w3.org/2000/svg" className={styles.checkmark} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" ><path d="M22 11.08V12a10 10 0 1 1-5.93-9.14" /><path d="m9 11 3 3L22 4" /></svg>
+                        {/* <svg xmlns="http://www.w3.org/2000/svg" className={styles.checkmark} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" ><path d="M22 11.08V12a10 10 0 1 1-5.93-9.14" /><path d="m9 11 3 3L22 4" /></svg> */}
+                        <svg xmlns="http://www.w3.org/2000/svg" className={styles.checkmark} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" stroke-linejoin="round" ><path d="M20 6 9 17l-5-5" /></svg>
                     </span>
                 </span>
             </label>
